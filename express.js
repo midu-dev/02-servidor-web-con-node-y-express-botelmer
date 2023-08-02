3377db422f87195c034ba14a8459de5521a9617e
Elmer EM. Mamani <elmer.mamani@REDSALUD.IQUIQUE.LOCAL>
2023-08-02 10:17:51 -0400

    Ejercicios terminados.

--- express.js
+++ express.js
@@ -1,7 +1,50 @@
+const express = require('express')
+const app = express()
+const fs = require('node:fs')
+const path = require('node:path')
+
 // Ejercicio 2: crear servidor HTTP con Express
 function startServer () {
-  // const server = app.listen(...)
-  // return server
+  app.get('/', (req, res) => {
+    res.send('<h1>¡Hola mundo!</h1>')
+  })
+
+  app.get('/logo.webp', (req, res) => {
+    const pathImg = path.join('assets', 'logo.webp')
+    const img = fs.readFileSync(pathImg, '')
+    res.header('Content-Type', 'image/webp').send(img)
+  })
+
+  app.get('/404', (req, res) => {
+    res.status(404).send('<h1>404</h1>')
+  })
+
+  app.post('/contacto', (req, res) => {
+    let body = ''
+
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    })
+
+    req.on('end', () => {
+      const data = JSON.parse(body)
+      res.status(201).json(data)
+    })
+  })
+
+  app.all('/', (req, res) => {
+    res.status(405).send('<h1>405 Method Not Allowed</h1>')
+  })
+
+  app.all('/contacto', (req, res) => {
+    res.status(405).send('<h1>405 Method Not Allowed</h1>')
+  })
+
+  const PORT = process.env.PORT ?? 1234
+  const server = app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+  })
+  return server
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,75 @@
+const http = require('node:http')
+const fs = require('node:fs')
+const path = require('node:path')
+
 // Ejercicio 1: crear servidor HTTP con Node
 function startServer () {
-  // return server
+  const processRequest = (req, res) => {
+    const { method, url } = req
+
+    switch (method) {
+      case 'GET': {
+        switch (url) {
+          case '/':
+            res.setHeader('Content-Type', 'text/html; charset=utf-8')
+            return res.end('<h1>¡Hola mundo!</h1>')
+
+          case '/logo.webp':{
+            const pathImg = path.join('assets', 'logo.webp')
+            res.setHeader('Content-Type', 'image/webp')
+            const img = fs.readFileSync(pathImg, '')
+            return res.end(img)
+          }
+          case '/404':
+            res.statusCode = 404
+            return res.end('<h1>404</h1>')
+
+          default:
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'text/html; charset=utf-8')
+            return res.end('<h1>404</h1>')
+        }
+      }
+
+      case 'POST':
+        switch (url) {
+          case '/contacto':{
+            let body = ''
+
+            req.on('data', (chunk) => {
+              body += chunk.toString()
+            })
+
+            req.on('end', () => {
+              const data = JSON.parse(body)
+              res.writeHead(201, {
+                'Content-Type': 'application/json; charset=utf-8'
+              })
+              return res.end(JSON.stringify(data))
+            })
+
+            break
+          }
+          default:
+            res.statusCode = 405
+            res.setHeader('Content-Type', 'text/html; charset=utf-8')
+            return res.end('<h1>405</h1>')
+        }
+        break
+
+      default:
+        res.statusCode = 405
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        return res.end('<h1>Método no soportado</h1>')
+    }
+  }
+
+  const PORT = process.env.PORT ?? 1234
+  const server = http.createServer(processRequest)
+  server.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+  })
+  return server
 }
 
 module.exports = {
